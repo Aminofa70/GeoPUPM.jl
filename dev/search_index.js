3bc73f723493dc90d2510698c97788aab850aad6
Documenter.jl <documenter@juliadocs.github.io>
2025-06-27 17:34:19 +0000

    build based on 80a4113

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeoPUPM","category":"page"},{"location":"#GeoPUPM","page":"Home","title":"GeoPUPM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeoPUPM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeoPUPM]","category":"page"}]
+[{"location":"#GeoPUPM","page":"Home","title":"GeoPUPM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeoPUPM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
